26b72c8b83b5c867e4d29b5e367a38ffd4b29664
blessedsoy <unmigong@gmail.com>
2016-10-12 13:17:02 -0400

    service

---
+++ www/js/services/people_service.js
@@ -0,0 +1,21 @@
+angular
+  .module('app')
+  .service("peopleService",function($window, $http){
+
+    this.create = function(name, phone) {
+      var url = "http://localhost:3000/api/people";
+      return $http({
+                method: 'POST',
+                url: url,
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json'
+                },
+                data: {
+                  name: name,
+                  phone: phone
+                }
+      });
+    };
+
+}
